a5f56ef008c2022cc16f32d467183c52959f4197
Muhammet <msolmaz83@gmail.com>
2024-11-08 11:43:10 +0100

    home1

--- ilbey/src/App.tsx
+++ ilbey/src/App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
--- ilbey/src/Home/Home.tsx
+++ ilbey/src/Home/Home.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import image from '../assets/1.jpg'
+import image from "../assets/1.jpg";
 
 const Home = () => {
   return (
-    <div>Home
-        <img src={image} alt=''/>
-        <img src={image} alt=''/>
+    <div className="derleme">
+      Home
+      <img src={image} alt="" className="derleme1" />
+      <p>Chicago-Based CPA Firm</p>
+      <p>
+        From bookkeeping and reporting to taxes and accounting, your finances
+        are in good hands with Lewis.cpa. Our full-service certified public
+        accounting firm takes care of everything, so you won’t have to.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
